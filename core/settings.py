33ae851f29a67fac9b6bde7cbc6705c6c714f06b
vanelleNgadjui <vanellejob@gmail.com>
2023-07-20 12:56:29 +0200

    vercel configuration

--- core/settings.py
+++ core/settings.py
@@ -122,11 +122,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "/static/"
+STATIC_URL = "/CaPotage/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build")
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/CaPotage/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
