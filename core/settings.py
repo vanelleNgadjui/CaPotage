b1463487dc5cb62d241a27a4d3bbeb15c4608050
vanelleNgadjui <vanellejob@gmail.com>
2023-07-25 01:59:02 +0200

    bientot je finis

--- core/settings.py
+++ core/settings.py
@@ -124,12 +124,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", 'static')
+STATIC_URL = "storages.backends.s3boto3.S3Boto3Storage/"
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", 'static')
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = "media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
